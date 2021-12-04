e612acf0154e02c3103a88f6b4d6eb392a8dc788
Mcszorongat <Mcsz@rongat.com>
2021-12-04 23:27:17 +0100

    CLEANUP: day1

--- day1.py
+++ day1.py
@@ -1,16 +1,31 @@
 import numpy as np
 
 
+def read_data(filename: str) -> np.ndarray:
+    with open(filename) as f:
+        return np.fromfile(f, dtype=int, count=-1, sep="\n")
+
+
+def task1(depths: np.ndarray) -> int:
+    return sum(np.diff(depths)>0)
+
+
+def rolling_sum(array: np.ndarray, window_width: int=3) -> np.ndarray:
+    temp = np.cumsum(array, dtype=int)
+    temp[window_width:] = temp[window_width:] - temp[:-window_width]
+    return temp[window_width - 1:]
+
+
+def task2(depths: np.ndarray, window_width: int=3) -> int:
+    depths_cum = rolling_sum(array=depths, window_width=window_width)
+    return sum(np.diff(depths_cum)>0)
+
+
 if __name__ == "__main__":
-    with open("input1.txt") as f:
-        depths = np.fromfile(f, dtype=int, count=-1, sep="\n")
+
+    depths = read_data("input1.txt")
     # depths = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
 
-    print("task1:\t", sum(np.diff(depths)>0))
+    print("task1:\t", task1(depths))
 
-    window_width = 3
-    new_length = len(depths) - window_width + 1
-    depths_cum = sum(
-        np.array([depths[i:new_length+i] for i in range(window_width)])
-    )
-    print("task2:\t", sum(np.diff(depths_cum)>0))
+    print("task2:\t", task2(depths))
