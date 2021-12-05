cfd07af946e3dcfb8d56aba7cb3872712d00e670
Mcszorongat <Mcsz@rongat.com>
2021-12-05 16:12:08 +0100

    ADDED: day5 - task2

--- day5.py
+++ day5.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy import matlib as ml
 
 
 def read_data(filename: str) -> np.ndarray:
@@ -11,32 +12,29 @@ def read_data(filename: str) -> np.ndarray:
     )
 
 
+def line_to_points(line: np.ndarray) -> np.ndarray:
+    p1 = line[0]
+    v = line[1] - line[0]
+    # v / np.abs(v).max() - only because of 45°
+    v_steps = ml.repmat(v / np.abs(v).max(), np.abs(v).max(), 1).cumsum(axis=0)
+    return np.append([p1], p1 + v_steps, axis=0)
+
+
 def task1(line_coordinates: np.ndarray) -> int:
     horz_vert_line_coordinates = line_coordinates[
-        [not all(line)
+        [any(line == 0)
          for line in (line_coordinates[:, 0] - line_coordinates[:, 1])]
     ]
+    points = np.vstack(
+        [line_to_points(line) for line in horz_vert_line_coordinates]
+    )
+    _, counts = np.unique(points, axis=0, return_counts=True)
+    return sum(counts > 1)
 
-    cloud_map = np.zeros([horz_vert_line_coordinates.max(),
-                          horz_vert_line_coordinates.max()])
-    
-    all_points = np.array([], dtype=int)
-    for line in horz_vert_line_coordinates:
-        const_mask = ((line[0] - line[1])==0) * np.array([1, 1])
-        varying_mask = np.matmul(const_mask, np.array([[0,1], [1,0]]))
-        offset = line[0] * const_mask
 
-        start = min(sum(line[0] - offset), sum(line[1] - offset))
-        end = max(sum(line[0] - offset), sum(line[1] - offset))
-        points = np.array(range(start, end + 1, 1))
-        raw_points = np.matmul(np.transpose([points]),
-                               np.atleast_2d(varying_mask))
-        abs_points = raw_points + offset
-        if len(all_points) != 0:
-            all_points = np.append(all_points, abs_points, axis=0)
-        else:
-            all_points = abs_points
-    _, counts = np.unique(all_points, axis=0, return_counts=True)
+def task2(line_coordinates: np.ndarray) -> int:
+    points = np.vstack([line_to_points(line) for line in line_coordinates])
+    _, counts = np.unique(points, axis=0, return_counts=True)
     return sum(counts > 1)
 
 
@@ -45,3 +43,5 @@ if __name__ == "__main__":
     line_coordinates = read_data("input5.txt")
 
     print("task1:\t", task1(line_coordinates=line_coordinates))
+
+    print("task2:\t", task2(line_coordinates=line_coordinates))
